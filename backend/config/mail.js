5284942318be71eaaed148deac6115f6508bfe17
Prabhath-P-Dev <prabhath0492@gmail.com>
2026-07-13 14:03:53 +0530

    nodemailer configuration updated

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,11 +1,16 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service:"gmail",
+    host:" smtp.gmail.com",
+    port:587,
+    secure:false,
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS,
     },
+    connectionTimeout:10000,
+    greetingTimeout:10000,
+    socketTimeout:10000,
 })
 
 export default transporter;
